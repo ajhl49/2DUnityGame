4bff066140aab5e05bbefba7fd71d3a595e64c47
DESKTOP-1VQR1SO\kenne <kennedy.lucas8900@gmail.com>
2017-01-11 16:23:48 -0600

    added basic movement and sample sprites

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingObject : MonoBehaviour {
+public class MovingObject : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float Speed = 4f;
+    public float SprintMult = 2f;
+    public bool Sprint = false;
+
+    private Vector3 currentDirection = Vector3.zero;
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentDirection = Vector3.left;
+            transform.position += currentDirection * Speed * (Sprint ? SprintMult : 1) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentDirection = Vector3.right;
+            transform.position += currentDirection * Speed * (Sprint ? SprintMult : 1) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            currentDirection = Vector3.up;
+            transform.position += currentDirection * Speed * (Sprint ? SprintMult : 1) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            currentDirection = Vector3.down;
+            transform.position += currentDirection * Speed * (Sprint ? SprintMult : 1) * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Sprint = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Sprint = false;
+        }
+
+    }
+   
 }
