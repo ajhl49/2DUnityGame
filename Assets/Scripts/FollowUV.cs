3bfcdb8376c502d4b8de796741396882a73f72fb
Aj Leino <leinoa@msoe.edu>
2017-01-13 18:08:57 -0600

    Replaced background with something more appropriate. Changed camera focus to player.

---
+++ Assets/Scripts/FollowUV.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUV : MonoBehaviour
+{
+    public float parralax = 2f;
+
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.GetTextureOffset("_MainTex");
+
+        offset.x = transform.position.x / transform.localScale.x / parralax;
+        offset.y = transform.position.y / transform.localScale.y / parralax;
+
+        mat.SetTextureOffset("_MainTex", offset);
+    }
+}
