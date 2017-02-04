486ba35674b77ecdfc683ba7f153c2cd35f034a2
Aj Leino <leinoa@msoe.edu>
2017-02-04 03:40:29 -0600

    Fixed stupid mistake

--- Assets/Scripts/QuitOnEnter.cs
+++ Assets/Scripts/QuitOnEnter.cs
@@ -1,15 +1,10 @@
-﻿using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class QuitOnEnter : StateMachineBehaviour {
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-#if UNITY_EDITOR
-	    EditorApplication.isPlaying = false;
-#else
         Application.Quit();
-#endif
     }
 }
